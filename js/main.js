c555e5fc0281d3bf35679b8a8880d7fafb2b529c
Alexa Schlegel <alexa.schlegel@inf.fu-berlin.de>
2012-02-22 02:18:33 +0100

    * writing a functions which gets the real selection (start and end pos) * it respects the snap to word function of the plugin wrapSelection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 function resetAppAfterAnnotation(){
 	// hover-effect for single words 
 	$(".word_split").lettering('words');
+	
+	$("#text .word_split span").mousedown(function(){
+		startElement = $(this);
+		console.log("startElement", startElement);
+	});
+	
+	$("#text .word_split span").mouseup(function(){
+		endElement = $(this);
+		console.log("endElement", endElement);
+	});
 }
 
 function loadCategoryMenu(){
@@ -27,5 +37,61 @@ function addAtomToAtomList(category, subcategory, start, end){
 		});
 }
 
-		
+/* As we are using two plugins to get the user selected text and to wrap
+ * a span around it, problems are arising. The selection plugin gets the
+ * exact position of the selected text within an element. The plugin who
+ * is wrapping spans around the user selection is "snapping" to complete
+ * words. So the start and end positions are not very accurate. Actually
+ * we need the exact start and end position to check for different kinds
+ * of overlaps like inclusion, identity and overlap.
+ * This function gets the start and end spans of the user selection (can
+ * be empty), selection(start-end position) and the plain selected text.
+ * It calculates the real selection range.
+ * */
+function getRealSelectionRange(startSpan, endSpan, selection, text){
+	
+	// there is an startSpan - if needed update it
+	if(startSpan){
+		selection = updateStart(startSpan, selection, text);	
+	}
+	
+	// there is an endSpan - if needed update it 
+	if(endSpan){
+		selection = updateEnd(endSpan, selection, text);
+	}
+	
+	// if there are no start/end - span, the selection is accurate
+	return selection;
+}
 
+/* helper function for getRealSelectionRange()*/
+function updateStart(startSpan, selection, text){
+	var startBit = text.split(" ")[0];
+	
+	if (endsWith(startSpan, startBit)){
+		var startOffset = startSpan.length - startBit.length;
+		selection["start"] = selection["start"] - startOffset;
+		return selection;
+	}
+}
+
+/* helper function for getRealSelectionRange()*/
+function updateEnd(endSpan, selection, text){
+	var endBit = text.split(" ")[text.split(" ").length-1];
+	
+	if (startsWith(endSpan, endBit)){
+		var endOffset = endSpan.length - endBit.length;
+		selection["end"] = selection["end"] + endOffset;
+		return selection;
+	}
+}
+
+// http://stackoverflow.com/questions/280634/endswith-in-javascript
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+// http://stackoverflow.com/questions/646628/javascript-startswith
+function startsWith(str, prefix){
+	return str.indexOf(prefix) == 0;
+}
