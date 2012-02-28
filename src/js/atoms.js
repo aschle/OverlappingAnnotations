52e9f35746b69958f16b6abf7905fe04760b56f7
Alexa Schlegel <alexa.schlegel@inf.fu-berlin.de>
2012-02-28 18:20:30 +0100

    * added some new functions to atom class

--- src/js/atoms.js
+++ src/js/atoms.js
@@ -1,15 +1,27 @@
 var Overlap = window.Overlap = Overlap || {};
-Overlap.atoms = {};
 
-Overlap.atoms.atomList = [];
 
-Overlap.atoms.addAtom = function(category, subcategory, start, end, id){
+Overlap.Atoms = function(){
 
-	Overlap.atoms.atomList.push({
-		"start":Number(start),
-		"end":Number(end),
-		"category":Number(category),
-		"subcategory":Number(subcategory),
-		"id":Number(id)
-		});
-}
+	var atomList = [];
+
+	this.addAtom = function(category, subcategory, start, end, id){
+
+		atomList.push({
+			"start"				: Number(start),
+			"end"					: Number(end),
+			"category"		: Number(category),
+			"subcategory"	: Number(subcategory),
+			"id"					: Number(id)
+			});
+	};
+
+	this.getAtomList = function(){
+		return atomList;
+	};
+
+	this.getLength = function(){
+		return atomList.length;
+	};
+
+};
