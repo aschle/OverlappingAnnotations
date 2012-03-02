1bc3e37c3eb2465d8e8bab5f6bf148ec637751b6
Alexa Schlegel <alexa.schlegel@inf.fu-berlin.de>
2012-03-02 02:13:52 +0100

    * implemented save and load issue (#3) * some new buttons and so on * using localStorage (key, value) with JSON to save the atomList * files can be saved with a new name or overridden * saved files can be loaded * names must be differnt and unique * maybe there should be a clear button * also somewhere should be a status thing, which showes to the user what thing he is editing

--- src/js/main.js
+++ src/js/main.js
@@ -18,12 +18,15 @@ $(document).ready(function() {
 
 	Overlap.savedClick		= null;
 	Overlap.textContent		= $("#text").clone();
+	Overlap.disabled			= false;
 
 	Overlap.Atoms 				= new Overlap.Atoms();
 	Overlap.Bar 					= new Overlap.Bar();
 	Overlap.Border 				= new Overlap.Border();
 	Overlap.Menu 					= new Overlap.Menu(Overlap.categories);
 	Overlap.activeConcept = Overlap.Bar;
+
+	Overlap.Storage				= new Overlap.Storage();
 
 	$("#barViewButton").addClass("activeButton");
 
@@ -77,9 +80,63 @@ $(document).ready(function() {
 		$(this).addClass("activeButton");
 	});
 
-	Overlap.textX = 250; 
+	Overlap.textX = 250;
 	// Does not work: $("#text").position().left;
 	Overlap.textY	= $("#text").position().top;
 
-	console.log(Overlap.textX);
+
+	// SAVE and LOAD Button
+	$("#saveButton").click(function(){
+		$("#savePopup").fadeIn(200);
+		$(".allInStorage").empty();
+		$("#saveName").val("");
+
+		Overlap.Helper.appendRadioList("radiosSave");
+
+
+		$("#saveName").keyup(function(){
+			if($(this).val().length > 0){
+				$("input[type=radio]").attr("disabled", "disabled");
+				Overlap.disabled = true;
+			} else {
+				$("input[type=radio]").removeAttr("disabled");
+				disabled = false;
+			}
+		});
+	});
+
+	$("#loadButton").click(function(){
+		$("#loadPopup").fadeIn(200);
+		$(".allInStorage").empty();
+		Overlap.Helper.appendRadioList("radiosLoad");
+	});
+
+	$("#confirmButtonSave").click(function(){
+		var name = null;
+		if(Overlap.disabled){
+			name = $("#saveName").val();
+		} else {
+				name=$("input[name=radiosSave]:checked").val();
+		}
+
+		console.log(name);
+
+		Overlap.Storage.save(name);
+		$("#savePopup").fadeOut(200);
+	});
+
+	$("#cancelButtonSave").click(function(){
+		$("#savePopup").fadeOut(200);
+	});
+
+	$("#confirmButtonLoad").click(function(){
+		var name = $("input[name=radiosLoad]:checked").val();
+		Overlap.Storage.load(name);
+		Overlap.activeConcept.run();
+		$("#loadPopup").fadeOut(200);
+	});
+
+	$("#cancelButtonLoad").click(function(){
+		$("#loadPopup").fadeOut(200);
+	});
 });
